6612de3179383fadc64bea2c82bfc1a3490b9fac
Feliciano <felicianogc@gmail.com>
2023-07-19 16:03:54 -0700

    App setup started

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View id="header" style={styles.header}></View>
+
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View id="footer" style={styles.header}></View>
     </View>
   );
 }
@@ -13,8 +16,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "space-between",
+  },
+  header: {
+    backgroundColor: "red",
+    height: 80,
+    width: "100%",
   },
 });
