850d672dae7a225f694f5093b37f206960014bbb
MariaFMartinez <mfmg0824@gmail.com>
2023-10-10 14:46:19 -0700

    Cambio_paleta1

--- theme/palette.js
+++ theme/palette.js
@@ -1,8 +1,8 @@
 export const palette = {
-  neutral: { dark: "#838383", medium: "#C3C3C3", light: "#E3E3E3" },
-  yellow: { dark: "#887C11", medium: "#C3CC0C", light: "#E2EB2C" },
-  orange: { dark: "#975416", medium: "#F5870A", light: "#F89F36" },
-  green: { dark: "#5E8811", medium: "#86C218", light: "#A0E522" },
-  skyBlue: { dark: "#115688", medium: "#4F9BBC", light: "#70C6EB" },
-  navyBlue: { dark: "#000B1C", medium: "#15324C", light: "#295474" },
+  neutral: { dark: "#A3A3A3", medium: "#C7C7C7", light: "#E3E3E3" },
+  yellow: { dark: "#B79608", medium: "#D7BE0D", light: "#E7D521" },
+  orange: { dark: "#C76D07", medium: "#F5870A", light: "#FFA323" },
+  green: { dark: "#6A9D0A", medium: "#85C70D", light: "#ABE33C" },
+  skyBlue: { dark: "#115688", medium: "#36A3D1", light: "#5EC8F4" },
+  navyBlue: { dark: "#01112A", medium: "#0A3A64", light: "#1366A4" },
 };
